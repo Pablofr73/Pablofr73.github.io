2595004a5240ebc7c80a70c44ce899ccf3161d8c
Pablo Franco <Pablofranc073@outlook.com>
2022-10-20 12:07:47 -0500

    Actualizacion de HtML y de archivos java en index

---
+++ notas.js
@@ -0,0 +1,5 @@
+alert('Este es una pagina en desarrollo, por si aun encuentran cosas incompletas, la pagina esta en ingles con información en español pero el objetivo es que en un futuro este toda en ingles.');
+alert('Si lees esto, te quiero mucho :D ');
+
+console.log ('Si encontraste esto es porque eres un metiche, que andas buscando en la consola.');
+
